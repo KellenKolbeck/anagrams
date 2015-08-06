a1efc0fc3e63203e727ca08fd062d7812fa0c47c
Josue Valverde & Kellen Kolbeck <student@epicodus.com>
2015-08-06 15:57:11 -0700

    Add functionality to anagram where it accepts a string as opposed to an array and update spec to fit this.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,6 +1,7 @@
 class String
-  define_method(:anagrams) do |words_list|
+  define_method(:anagrams) do |words_string|
     split_word = self.split("").sort()
+    words_list = words_string.split(",")
     anagram_list = []
     words_list.each() do |words|
       if split_word.eql?(words.split("").sort())
