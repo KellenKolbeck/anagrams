4b88c5628bda731f31a71d78606e88536db60764
Josue Valverde & Kellen Kolbeck <student@epicodus.com>
2015-08-06 16:27:54 -0700

    Add specs that fill in data and check that correct page and output is shown.

--- spec/anagrams_integration_spec.rb
+++ spec/anagrams_integration_spec.rb
@@ -2,3 +2,17 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('/', {:type => :feature}) do
+  it("loads the index page correctly") do
+    visit('/')
+    expect(page).to have_content("Your word")
+  end
+  it("returns the correct list of anagrams for a word") do
+    visit('/')
+    fill_in('user_word', :with => "fox")
+    fill_in('words', :with =>"oxf,xof,puppy")
+    click_button("Go!")
+    expect(page).to have_content("oxf, xof")
+  end
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,10 +3,10 @@ require('anagrams')
 
 describe('String#anagrams') do
   it("takes the input and returns if it is an anagram") do
-    expect(("fox").anagrams("xof")).to(eq(["xof"]))
+    expect(("fox").anagrams("xof")).to(eq("xof"))
   end
 
   it("takes the inputs and returns which ones are anagrams") do
-    expect(("fox").anagrams("xof,xfo,oxf,ofx,fxo,dog,burrito")).to(eq(["xof", "xfo", "oxf", "ofx", "fxo"]))  
+    expect(("fox").anagrams("xof,xfo,oxf,ofx,fxo,dog,burrito")).to(eq("xof, xfo, oxf, ofx, fxo"))  
   end
 end
