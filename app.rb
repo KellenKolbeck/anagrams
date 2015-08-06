9d85dad6b92a105ef730dc2947fceed17299aea0
Josue Valverde & Kellen Kolbeck <student@epicodus.com>
2015-08-06 16:28:44 -0700

    Catch variables from index page and write code to fetch them for /words(solution) page.

--- app.rb
+++ app.rb
@@ -1,2 +1,15 @@
+require('sinatra')
+require('sinatra/reloader')
 require('./lib/anagrams')
-also_reload('/lib/**/*.rb')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/words') do
+  @user_word = params.fetch('user_word')
+  @words = params.fetch('words')
+  @anagram_list = @user_word.anagrams(@words)
+  erb(:words)
+end
