2821a32f45bed5ea41f511006e7d1e285c248a14
Josue Valverde & Kellen Kolbeck <student@epicodus.com>
2015-08-06 15:14:28 -0700

    Add and pass second spec that returns all words that are anagrams.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,10 +1,15 @@
 class String
-  define_method(:anagrams) do |letters|
-    split_word = self.split("")
-    if split_word.sort().eql?(letters[0].split("").sort())
-      letters
-    else
+  define_method(:anagrams) do |words_list|
+    split_word = self.split("").sort()
+    anagram_list = []
+    words_list.each() do |words|
+      if split_word.eql?(words.split("").sort())
+        anagram_list.push(words)
+      end
+    end
+    if !anagram_list
       "No anagrams found"
     end
+    anagram_list
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,4 +5,8 @@ describe('String#anagrams') do
   it("takes the input and returns if it is an anagram") do
     expect(("fox").anagrams(["xof"])).to(eq(["xof"]))
   end
+
+  it("takes the inputs and returns which ones are anagrams") do
+    expect(("fox").anagrams(["xof", "xfo", "oxf", "ofx", "fxo", "dog", "burrito"])).to(eq(["xof", "xfo", "oxf", "ofx", "fxo"]))  
+  end
 end
